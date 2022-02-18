e782f4a543469d93b229e41cb9d6e25ba1dc47a6
King Khan <nsikan.na@gmail.com>
2022-02-18 01:59:26 -0500

    add to card button adds item to stores cart and updates running total

--- components/SignUpModule.js
+++ components/SignUpModule.js
@@ -58,6 +58,7 @@ export default function SignUpModule() {
                   e.preventDefault();
                   setSignInModule(true);
                   setSignUpModule(false);
+                  
                 }}
               >
                 Sign In!
