75bae313f9779be1934a5ab5a7eed80e2da53599
King Khan <nsikan.na@gmail.com>
2022-02-25 15:08:03 -0500

    improved footer to display running total/ location

--- components/Footer.js
+++ components/Footer.js
@@ -3,17 +3,27 @@ import Container from "react-bootstrap/Container";
 import { Context } from "../context";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Button from "react-bootstrap/Button";
+
 export default function Footer() {
-  const {
-    location,
-    runningTotal,
-    cart,
-    setCartModule,
-  } = useContext(Context);
+  const { location, runningTotal, cart, setCartModule } = useContext(Context);
 
   return (
-    <Container className="text-center sticky bottom-0 bg-white">
-      <p> Gwinnett Dinner {location?`@ ${location}`:''}</p>
+    <Container className="text-center sticky bottom-0 bg-white  py-2">
+      {cart.length!==0?<div className="flex justify-center items-center space-x-10">
+        <h5>${runningTotal}</h5>
+        <Button
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            setCartModule(true);
+          }}
+        >
+          Checkout
+        </Button>
+      </div>:
+      <div className="mt-3">
+        <h6 className="">Gwinnett Dinner {location ? `@ ${location}` : ""}</h6>
+      </div>}
     </Container>
   );
 }
