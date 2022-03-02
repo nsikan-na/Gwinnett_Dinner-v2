eaffd193d2320a71bf9b2c19e2997f267b126782
King Khan <nsikan.na@gmail.com>
2022-03-02 00:22:27 -0500

    test

--- pages/api/sign-up.js
+++ pages/api/sign-up.js
@@ -1,27 +1,31 @@
-import db from "./db";
+import { MongoClient } from "mongodb";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const data = req.body;
       const { username, password, confirmPassword } = data;
       if (!username || !password) {
-        return res.send({
+        return res.json({
           success: false,
           message: "Please complete all fields!",
         });
       }
       if (password != confirmPassword) {
-        return res.send({
+        return res.json({
           success: false,
           message: "Passwords do not match!",
         });
       }
-      await db.execute(
-        `INSERT INTO user_data (username,password) VALUES("${username}","${password}")`
+      const client = await MongoClient.connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ewgfl.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
       );
-      return res.send({ success: true });
+      const db = client.db();
+      const userDataCollection = db.collection(`${process.env.DB_COLLECTION}`);
+      const result = await userDataCollection.insertOne({ username, password });
+      if (result.acknowledged) return res.json({ success: true });
+      client.close();
     } catch (err) {
-      console.log(error.message);
+      console.log(err.message);
     }
   }
 }
